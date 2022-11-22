54a6738a08b434d455c5e2a26359689703d18340
shosha98 <118348325+shosha98@users.noreply.github.com>
2022-11-22 16:16:28 +0300

    h
    h

--- Html/aljanoubia/assets/js/table-data.js
+++ Html/aljanoubia/assets/js/table-data.js
@@ -1,18 +1,18 @@
-$(function () {
-   'use strict'
+// $(function () {
+//    'use strict'
 
-   //Data table example
-   var table = $('#exportexample').DataTable({
+//    //Data table example
+//    var table = $('#exportexample').DataTable({
       
-      lengthChange: true,
-      language: {
-         searchPlaceholder: 'ابحث هنا ...',
-         sSearch: '',
-         lengthMenu: '_MENU_ عناصر/صفحة',
-      },
-      buttons: ['copy', 'excel', 'pdfHtml5','print', 'colvis']
-   });
-   table.buttons().container()
-      .appendTo('#exportexample_wrapper .col-md-6:eq(0)');
+//       lengthChange: true,
+//       language: {
+//          searchPlaceholder: 'ابحث هنا ...',
+//          sSearch: '',
+//          lengthMenu: '_MENU_ عناصر/صفحة',
+//       },
+//       buttons: ['copy', 'excel', 'pdfHtml5','print', 'colvis']
+//    });
+//    table.buttons().container()
+//       .appendTo('#exportexample_wrapper .col-md-6:eq(0)');
 
-});
+// });
